302b411eb927f5e617524fbed467466fab7f44a5
CosmoBlack <soulker_mozyr@yahoo.com>
2023-04-19 21:15:58 +0200

    Initial commit

---
+++ src/test/java/com/example/homework216/Homework216ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework216;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework216ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
